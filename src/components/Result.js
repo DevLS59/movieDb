613a46942a2724637a0c3b1e5eb9c28efeb0b966
devls <devls@yahoo.com>
2020-01-07 19:49:43 +0100

    add image default

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,11 +1,11 @@
 import React from 'react'
-
+import imageDefault  from '../images/cinema-illustr1.jpg'
 const Result = ( { result, openPopup }) => {
 
-    // console.log(result)
+    console.log(result.Poster)
     return(
         <div className="result" onClick={() => openPopup(result.imdbID)}>
-            <img src={result.Poster} alt="poster"/>
+          {  (result.Poster !== "N/A") ?  <img src={result.Poster} alt="poster"/> : <img src={imageDefault} alt="poster"/> }
             <h3> {result.Title } </h3>    
         </div>
     )
